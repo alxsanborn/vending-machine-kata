0d0065091c471cefead907980411a0c0689c1532
jasmin47 <alxsanborn@gmail.com>
2017-01-04 11:25:52 -0500

    add validations context for tests requiring weight, diameter, and thickness

--- spec/models/coin_spec.rb
+++ spec/models/coin_spec.rb
@@ -6,18 +6,21 @@ RSpec.describe Coin, type: :model do
     expect(coin).to be_valid
   end
 
-  it 'requires weight attribute' do
-    no_weight = Coin.create(diameter: 3.2, thickness: 0.9)
-    expect(no_weight).to_not be_valid
-  end
+  context 'validations' do
+    it 'requires weight attribute' do
+      no_weight = Coin.create(diameter: 3.2, thickness: 0.9)
+      expect(no_weight).to_not be_valid
+    end
 
-  it 'requires diameter attribute' do
-    no_diameter = Coin.create(weight: 3.3, thickness: 4.4)
-    expect(no_diameter).to_not be_valid
-  end
+    it 'requires diameter attribute' do
+      no_diameter = Coin.create(weight: 3.3, thickness: 4.4)
+      expect(no_diameter).to_not be_valid
+    end
 
-  it 'requires thickness attribute' do
-    no_thickness = Coin.create(weight: 1.2, diameter: 3.4)
-    expect(no_thickness).to_not be_valid
+    it 'requires thickness attribute' do
+      no_thickness = Coin.create(weight: 1.2, diameter: 3.4)
+      expect(no_thickness).to_not be_valid
+    end
   end
+  
 end
