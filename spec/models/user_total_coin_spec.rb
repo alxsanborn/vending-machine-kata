305a4871b32f1721d9fe9855ca51f08e1c0ac6df
alxsanborn <alxsanborn@gmail.com>
2017-01-05 17:59:17 -0500

    identify and add nickels to a user's total coins

--- spec/models/user_total_coin_spec.rb
+++ spec/models/user_total_coin_spec.rb
@@ -30,4 +30,10 @@ RSpec.describe UserTotalCoin, type: :model do
     total_coins.evaluate_coin_values(dime)
     expect(total_coins.dimes.length).to eq(1)
   end
+
+  it 'can identify and add a nickel to a user`s total coins' do
+    nickel = Coin.create(diameter: 0.835, weight: 5.000, thickness: 0.1625)
+    total_coins.evaluate_coin_values(nickel)
+    expect(total_coins.nickels.length).to eq(1)
+  end
 end
