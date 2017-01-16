6b69271037b401ce23b90b12cfb65080ed381981
alxsanborn <alxsanborn@gmail.com>
2017-01-16 18:28:33 -0500

    refactor product rspec tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,47 +4,59 @@ RSpec.describe Product, type: :model do
 
   let (:coffee) {Product.create(name: "coffee", price: 1.00)}
 
-  it 'can create a new product with name and price' do
-    expect(coffee).to be_valid
+  context 'validations' do
+    it 'can create a new product with name and price' do
+      expect(coffee).to be_valid
+    end
+
+    it 'cannot be created without name' do
+      product_1 = Product.create(price: 0.45)
+      expect(product_1).to_not be_valid
+    end
+
+    it 'cannot be created without price' do
+      free_cheetos = Product.create(name: 'cheetos')
+      expect(free_cheetos).to_not be_valid
+    end
   end
 
-  it 'selected attribute defaults to false' do
-    expect(coffee.selected).to be(false)
+  context 'default values' do
+    it 'selected attribute defaults to false' do
+      expect(coffee.selected).to be(false)
+    end
+
+    it 'quantity defaults to 0' do
+      expect(coffee.quantity).to eq(0)
+    end
   end
 
-  it 'cannot be created without name' do
-    product_1 = Product.create(price: 0.45)
-    expect(product_1).to_not be_valid
+  describe '#select_button' do
+    it 'user can select a product' do
+      coffee.select_button
+      expect(coffee.selected).to be(true)
+    end
   end
 
-  it 'cannot be created without price' do
-    free_cheetos = Product.create(name: 'cheetos')
-    expect(free_cheetos).to_not be_valid
+  describe '.product_selected?' do
+    it 'can detect which products are selected' do
+      coffee.select_button
+      expect(Product.product_selected?).to eq(coffee)
+    end
   end
 
-  it 'user can select a product' do
-    coffee.select_button
-    expect(coffee.selected).to be(true)
+  describe '#deselect_button' do
+    it 'can deselect a product' do
+      coffee.select_button
+      coffee.deselect_button
+      expect(coffee.selected).to be(false)
+    end
   end
 
-  it 'quantity defaults to 0' do
-    expect(coffee.quantity).to eq(0)
-  end
-
-  it 'can detect which products are selected' do
-    coffee.select_button
-    expect(Product.product_selected?).to eq(coffee)
-  end
-
-  it 'can deselect a product' do
-    coffee.select_button
-    coffee.deselect_button
-    expect(coffee.selected).to be(false)
-  end
-
-  it 'can decrease quantity' do
-    coffee.update(quantity: 6)
-    coffee.decrease_product_quantity
-    expect(coffee.quantity).to eq(5)
+  describe '#decrease_product_quantity' do
+    it 'can decrease quantity' do
+      coffee.update(quantity: 6)
+      coffee.decrease_product_quantity
+      expect(coffee.quantity).to eq(5)
+    end
   end
 end
