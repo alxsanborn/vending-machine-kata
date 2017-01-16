1035929175a9ec50a3271f538dfac8ed74ce7502
alxsanborn <alxsanborn@gmail.com>
2017-01-15 21:30:59 -0500

    refactor coin and inserted coin rspec tests

--- spec/models/coin_spec.rb
+++ spec/models/coin_spec.rb
@@ -2,19 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Coin, type: :model do
 
-  before (:each) do
-    @valid_coins = InsertedCoin.create
-    @dime_1 = Coin.create(diameter: 0.705, weight: 2.269, thickness: 0.053)
-    @dime_2 = Coin.create(diameter: 0.705, weight: 2.269, thickness: 0.053)
-    @dime_3 = Coin.create(diameter: 0.705, weight: 2.269, thickness: 0.053)
-    @quarter_1 = Coin.create(diameter: 0.954, thickness: 0.069, weight: 5.671)
-    @quarter_2 = Coin.create(diameter: 0.954, thickness: 0.069, weight: 5.671)
-    @nickel = Coin.create(diameter: 0.835, weight: 5.000, thickness: 0.1625)
-    @penny = Coin.create(diameter: 0.751, weight: 2.500, thickness: 1.52)
-  end
-
+  let (:valid_coins) {InsertedCoin.create}
+  let (:dime_1) {Coin.create(diameter: 0.705, weight: 2.269, thickness: 0.053)}
+  let (:dime_2) {Coin.create(diameter: 0.705, weight: 2.269, thickness: 0.053)}
+  let (:dime_3) {Coin.create(diameter: 0.705, weight: 2.269, thickness: 0.053)}
+  let (:quarter_1) {Coin.create(diameter: 0.954, thickness: 0.069, weight: 5.671)}
+  let (:quarter_2) {Coin.create(diameter: 0.954, thickness: 0.069, weight: 5.671)}
+  let (:nickel) {Coin.create(diameter: 0.835, weight: 5.000, thickness: 0.1625)}
+  let (:penny) {Coin.create(diameter: 0.751, weight: 2.500, thickness: 1.52)}
   it 'can create a new coin' do
-    expect(@dime_1).to be_valid
+    expect(dime_1).to be_valid
   end
 
   context 'validations' do
@@ -35,70 +32,70 @@ RSpec.describe Coin, type: :model do
   end
 
   it 'can add dimes' do
-    @dime_1.dime(@valid_coins)
-    expect(@valid_coins.dimes).to eq(1)
+    dime_1.dime(valid_coins)
+    expect(valid_coins.dimes).to eq(1)
   end
 
   it 'can add nickels' do
-    @nickel.nickel(@valid_coins)
-    expect(@valid_coins.nickels).to eq(1)
+    nickel.nickel(valid_coins)
+    expect(valid_coins.nickels).to eq(1)
   end
 
   it 'can add quarters' do
-    @quarter_1.quarter(@valid_coins)
-    expect(@valid_coins.quarters).to eq(1)
+    quarter_1.quarter(valid_coins)
+    expect(valid_coins.quarters).to eq(1)
   end
 
   it 'can identify and add a dime to a user`s total coins' do
-    @dime_1.evaluate_coin_values(@valid_coins)
-    expect(@valid_coins.dimes).to eq(1)
+    dime_1.evaluate_coin_values(valid_coins)
+    expect(valid_coins.dimes).to eq(1)
   end
 
   it 'can identify and add a nickel to a user`s total coins' do
-    @nickel.evaluate_coin_values(@valid_coins)
-    expect(@valid_coins.nickels).to eq(1)
+    nickel.evaluate_coin_values(valid_coins)
+    expect(valid_coins.nickels).to eq(1)
   end
 
   it 'can identify and add a quarter to a user`s total coins' do
-    @quarter_1.evaluate_coin_values(@valid_coins)
-    expect(@valid_coins.quarters).to eq(1)
+    quarter_1.evaluate_coin_values(valid_coins)
+    expect(valid_coins.quarters).to eq(1)
   end
 
   it 'can return the total value of coins that a user has inserted' do
-    @dime_1.evaluate_coin_values(@valid_coins)
-    @dime_2.evaluate_coin_values(@valid_coins)
-    @dime_3.evaluate_coin_values(@valid_coins)
-    @quarter_1.evaluate_coin_values(@valid_coins)
-    @quarter_2.evaluate_coin_values(@valid_coins)
-    @nickel.evaluate_coin_values(@valid_coins)
-    @penny.evaluate_coin_values(@valid_coins)
+    dime_1.evaluate_coin_values(valid_coins)
+    dime_2.evaluate_coin_values(valid_coins)
+    dime_3.evaluate_coin_values(valid_coins)
+    quarter_1.evaluate_coin_values(valid_coins)
+    quarter_2.evaluate_coin_values(valid_coins)
+    nickel.evaluate_coin_values(valid_coins)
+    penny.evaluate_coin_values(valid_coins)
 
-    expect(@valid_coins.total).to eq(0.85)
+    expect(valid_coins.total).to eq(0.85)
 end
 
   it 'rejects objects with valid diameter and thickness but invalid weight' do
    invalid_dime = Coin.create(weight: 2.500, diameter: 0.705, thickness: 0.053)
-   invalid_dime.evaluate_coin_values(@valid_coins)
-   expect(@valid_coins.quarters).to eq(0)
-   expect(@valid_coins.nickels).to eq(0)
-   expect(@valid_coins.dimes).to eq(0)
+   invalid_dime.evaluate_coin_values(valid_coins)
+   expect(valid_coins.quarters).to eq(0)
+   expect(valid_coins.nickels).to eq(0)
+   expect(valid_coins.dimes).to eq(0)
 
   end
 
   it 'rejects objects with valid weight and thickness but invalid diameter' do
    invalid_nickel = Coin.create(weight: 5.000, diameter: 3.000, thickness: 1.950)
-   invalid_nickel.evaluate_coin_values(@valid_coins)
-   expect(@valid_coins.quarters).to eq(0)
-   expect(@valid_coins.nickels).to eq(0)
-   expect(@valid_coins.dimes).to eq(0)
+   invalid_nickel.evaluate_coin_values(valid_coins)
+   expect(valid_coins.quarters).to eq(0)
+   expect(valid_coins.nickels).to eq(0)
+   expect(valid_coins.dimes).to eq(0)
   end
 
   it 'rejects objects with valid weight and diameter but invalid thickness' do
    invalid_quarter = Coin.create(weight: 5.561, diameter: 0.955, thickness: 0.066)
-   invalid_quarter.evaluate_coin_values(@valid_coins)
-   expect(@valid_coins.quarters).to eq(0)
-   expect(@valid_coins.nickels).to eq(0)
-   expect(@valid_coins.dimes).to eq(0)
+   invalid_quarter.evaluate_coin_values(valid_coins)
+   expect(valid_coins.quarters).to eq(0)
+   expect(valid_coins.nickels).to eq(0)
+   expect(valid_coins.dimes).to eq(0)
   end
 
 end
--- spec/models/inserted_coin_spec.rb
+++ spec/models/inserted_coin_spec.rb
@@ -1,45 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe InsertedCoin, type: :model do
-   before(:each) do
-     @inserted_coins = InsertedCoin.create
-     @dime_1 = Coin.create(diameter: 0.705, weight: 2.269, thickness: 0.053)
-     @dime_2 = Coin.create(diameter: 0.705, weight: 2.269, thickness: 0.053)
-     @dime_3 = Coin.create(diameter: 0.705, weight: 2.269, thickness: 0.053)
-     @quarter_1 = Coin.create(diameter: 0.954, thickness: 0.069, weight: 5.671)
-     @quarter_2 = Coin.create(diameter: 0.954, thickness: 0.069, weight: 5.671)
-     @nickel = Coin.create(diameter: 0.835, weight: 5.000, thickness: 0.1625)
-     @penny = Coin.create(diameter: 0.751, weight: 2.500, thickness: 1.52)
-   end
+
+  let (:inserted_coins) {InsertedCoin.create}
+  let (:dime_1) {Coin.create(diameter: 0.705, weight: 2.269, thickness: 0.053)}
+  let (:dime_2) {Coin.create(diameter: 0.705, weight: 2.269, thickness: 0.053)}
+  let (:dime_3) {Coin.create(diameter: 0.705, weight: 2.269, thickness: 0.053)}
+  let (:quarter_1) {Coin.create(diameter: 0.954, thickness: 0.069, weight: 5.671)}
+  let (:quarter_2) {Coin.create(diameter: 0.954, thickness: 0.069, weight: 5.671)}
+  let (:nickel) {Coin.create(diameter: 0.835, weight: 5.000, thickness: 0.1625)}
+  let (:penny) {Coin.create(diameter: 0.751, weight: 2.500, thickness: 1.52)}
 
   it 'returns a value of 0 if no coins have been added' do
-    expect(@inserted_coins.total).to eq(0)
+    expect(inserted_coins.total).to eq(0)
   end
 
   it 'returns the value of coins when one coin has been added' do
-    @dime_1.evaluate_coin_values(@inserted_coins)
-    expect(@inserted_coins.total).to eq(0.10)
+    dime_1.evaluate_coin_values(inserted_coins)
+    expect(inserted_coins.total).to eq(0.10)
   end
 
   it 'returns the value of coins when two different coins have been added' do
-    @dime_1.evaluate_coin_values(@inserted_coins)
-    @quarter_1.evaluate_coin_values(@inserted_coins)
-    expect(@inserted_coins.total).to eq(0.35)
+    dime_1.evaluate_coin_values(inserted_coins)
+    quarter_1.evaluate_coin_values(inserted_coins)
+    expect(inserted_coins.total).to eq(0.35)
   end
 
   it 'returns the value of coins when three different coins have been added' do
-    @dime_1.evaluate_coin_values(@inserted_coins)
-    @quarter_1.evaluate_coin_values(@inserted_coins)
-    @nickel.evaluate_coin_values(@inserted_coins)
-    expect(@inserted_coins.total).to eq(0.40)
+    dime_1.evaluate_coin_values(inserted_coins)
+    quarter_1.evaluate_coin_values(inserted_coins)
+    nickel.evaluate_coin_values(inserted_coins)
+    expect(inserted_coins.total).to eq(0.40)
   end
 
   it 'returns the value of coins when multiple coins of the same kind have been added' do
-    @dime_1.evaluate_coin_values(@inserted_coins)
-    @dime_2.evaluate_coin_values(@inserted_coins)
-    @quarter_1.evaluate_coin_values(@inserted_coins)
-    @quarter_2.evaluate_coin_values(@inserted_coins)
-    @nickel.evaluate_coin_values(@inserted_coins)
-    expect(@inserted_coins.total).to eq(0.75)
+    dime_1.evaluate_coin_values(inserted_coins)
+    dime_2.evaluate_coin_values(inserted_coins)
+    quarter_1.evaluate_coin_values(inserted_coins)
+    quarter_2.evaluate_coin_values(inserted_coins)
+    nickel.evaluate_coin_values(inserted_coins)
+    expect(inserted_coins.total).to eq(0.75)
   end
 end
