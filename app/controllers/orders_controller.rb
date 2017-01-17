d2a95ff1be8eb6673003b954df9b3b8e88e54c82
alxsanborn <alxsanborn@gmail.com>
2017-01-17 15:47:09 -0500

    refactor orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,8 @@
 class OrdersController < ApplicationController
   def new
-    @order = Order.new
   end
 
   def create
-    if @order.save
-      @order.inserted_coin.push(InsertedCoin.create)
-    else
-      render :new
-    end
-  end  
+    @order.inserted_coin.push(InsertedCoin.create)
+  end
 end
