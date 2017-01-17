580ed3a67e8f6c26b9e3391e2d85c3e1746b884b
alxsanborn <alxsanborn@gmail.com>
2017-01-17 15:34:55 -0500

    add create action to orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,13 @@
 class OrdersController < ApplicationController
+  def new
+    @order = Order.new
+  end
+
+  def create
+    if @order.save
+      @order.inserted_coin.push(InsertedCoin.create)
+    else
+      render :new
+    end
+  end  
 end
