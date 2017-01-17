ee73e19d5d2368c78d6e937331d0f0e50550b3e3
alxsanborn <alxsanborn@gmail.com>
2017-01-17 16:17:56 -0500

    create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+cola = Product.create(name: "cola", price: 1.00, quantity: 30)
+chips = Product.create(name: "chips", price: 0.50)
+candy = Product.create(name: "candy", price: 0.65, quantity: 5)
+
+order = Order.create
+
+quarter_1 = Coin.create(diameter: 0.954, thickness: 0.069, weight: 5.671)
+quarter_2 = Coin.create(diameter: 0.954, thickness: 0.069, weight: 5.671)
+quarter_3 = Coin.create(diameter: 0.954, thickness: 0.069, weight: 5.671)
+quarter_4 = Coin.create(diameter: 0.954, thickness: 0.069, weight: 5.671)
+
+dime_1 = Coin.create(diameter: 0.705, weight: 2.269, thickness: 0.053)
+dime_2 = Coin.create(diameter: 0.705, weight: 2.269, thickness: 0.053)
+dime_3 = Coin.create(diameter: 0.705, weight: 2.269, thickness: 0.053)
+dime_4 = Coin.create(diameter: 0.705, weight: 2.269, thickness: 0.053)
+dime_5 = Coin.create(diameter: 0.705, weight: 2.269, thickness: 0.053)
+dime_6 = Coin.create(diameter: 0.705, weight: 2.269, thickness: 0.053)
+
+nickel_1 = Coin.create(diameter: 0.835, weight: 5.000, thickness: 0.1625)
+nickel_2 = Coin.create(diameter: 0.835, weight: 5.000, thickness: 0.1625)
+nickel_3 = Coin.create(diameter: 0.835, weight: 5.000, thickness: 0.1625)
+nickel_4 = Coin.create(diameter: 0.835, weight: 5.000, thickness: 0.1625)
+nickel_5 = Coin.create(diameter: 0.835, weight: 5.000, thickness: 0.1625)
+nickel_6 = Coin.create(diameter: 0.835, weight: 5.000, thickness: 0.1625)
+
+penny = Coin.create(diameter: 0.751, weight: 2.500, thickness: 1.52)
