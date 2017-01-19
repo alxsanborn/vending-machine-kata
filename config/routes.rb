14398c5421ae113efba5b6fa0d1641f2b1c567d7
alxsanborn <alxsanborn@gmail.com>
2017-01-18 20:35:28 -0500

    delete route for editing a product

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :products, except: :destroy
+  resources :products, except: [:destroy, :edit, :update]
   resources :orders, only: :show
 end
