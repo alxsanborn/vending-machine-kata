6247c141ab922ee3dd39dfa6a5e5d045750adff8
alxsanborn <alxsanborn@gmail.com>
2017-01-19 18:38:10 -0500

    create order and link new session to order id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    order = Order.create
+    session[:order_id] = order.id
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,8 +1,22 @@
+require 'rails_helper'
+require 'pry'
+
 RSpec.describe SessionsController, :type => :routing do
   it "routes to /sessions/new" do
     expect(:get => "/sessions/new").to be_routable
   end
   it "routes to /sessions/create" do
     expect(:post => "/sessions").to be_routable
+  end
+end
+
+RSpec.describe SessionsController, :type => :controller do
+  context 'create new session' do
+    it "initializes with new order id" do
+      expect(Order.count).to eq(0)
+      post :create
+      expect(request.session["order_id"]).to eq(1)
+      expect(Order.count).to eq(1)
+    end
   end
 end
