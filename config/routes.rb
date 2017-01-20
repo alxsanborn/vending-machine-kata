23b1caa0cbdc4d555329a393144558cf18ad6a4f
alxsanborn <alxsanborn@gmail.com>
2017-01-20 16:10:36 -0500

    add edit route coin controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :products, except: [:destroy, :edit, :update]
   resources :orders, only: [:show, :new, :create]
-  resources :coins, only: [:show, :new, :create]
+  resources :coins, only: [:show, :new, :create, :edit]
   resources :inserted_coins, except: :destroy
   resources :sessions, only: [:new, :create]
 
--- spec/controllers/coins_controller_spec.rb
+++ spec/controllers/coins_controller_spec.rb
@@ -12,4 +12,8 @@ RSpec.describe CoinsController, :type => :routing do
   it "routes to /coins/create" do
     expect(:post => "/coins").to be_routable
   end
+
+  it "routes to /coins/:id/edit" do
+    expect(:get => "/coins/:id/edit").to be_routable
+  end
 end
