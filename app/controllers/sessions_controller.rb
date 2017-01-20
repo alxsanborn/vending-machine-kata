7a26e0170bca7f265b34a9bb8b2edddc2a2c6513
alxsanborn <alxsanborn@gmail.com>
2017-01-20 13:02:12 -0500

    refactor sessions processes

--- app/controllers/sessions_controller.rb
+++
@@ -1,9 +0,0 @@
-class SessionsController < ApplicationController
-  def new
-  end
-
-  def create
-    order = Order.create(inserted_coin: InsertedCoin.create)
-    session[:order_id] = order.id
-  end
-end
