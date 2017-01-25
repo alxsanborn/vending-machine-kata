158affe449a8407401b113a6d0756e98aded230c
alxsanborn <alxsanborn@gmail.com>
2017-01-25 13:08:44 -0500

    refactor product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,4 @@
-#I used Ruby because it's the language I'm most comfortable in, but after doing this project, I'm not sure Rails was the best framework to use. I probably should have just done a CLI, but by the time I realized this, it was too late because I had already done so much work on the front end. Rails favors convention over configuration. This particular project had a lot of custom logic that probably would have been a better fit for a framework like Node.js that favors configuration over convention.  
+#I used Ruby because it's the language I'm most comfortable in, but after doing this project, I'm not sure Rails was the best framework to use. I probably should have just done a CLI, but by the time I realized this, it was too late because I had already done so much work on the front end. Rails favors convention over configuration. This particular project had a lot of custom logic that probably would have been a better fit for a framework like Node.js that favors configuration over convention.
 class ProductsController < ApplicationController
   def new
   end
@@ -7,7 +7,7 @@ class ProductsController < ApplicationController
   end
 
   def index
-    current_order
+    current_order #associates session with instance of order
     @products = Product.all
     @coins = Coin.all
   end
@@ -17,16 +17,8 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    if params["id"] == ":id"
-      @product = Product.product_selected?
-    else @product = Product.find(params[:id])
-    end
-
-    if @product.selected == false
-      @product.select_button
-    else
-      @product.deselect_button
-    end
+    params["id"] == ":id" ? @product = Product.product_selected? : @product = Product.find(params[:id])
+    @product.selected == false ? @product.select_button : @product.deselect_button
     redirect_to root_path
   end
 end
