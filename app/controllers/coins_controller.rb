3e8ec8d6f67bf2feca08e0c757c5e9b9f8aca703
alxsanborn <alxsanborn@gmail.com>
2017-01-25 01:07:40 -0500

    debug coin amount reset on coin return

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -9,7 +9,10 @@ class CoinsController < ApplicationController
   end
 
   def edit
-    current_order.update(return_coins: false)
+    if current_order.return_coins == true
+      current_order.update(return_coins: false)
+      current_order.reset_coin_amounts
+    end
     coin = Coin.find(params[:id])
     coin.evaluate_coin_values(current_order.inserted_coin)
     redirect_to root_path
